5550520badf1a9d6c3eb2bad2ce5e02776a62fed
msfukui <msfukui@gmail.com>
2019-12-21 20:10:13 +0900

    Add a temporary implemetation for test
    レシーバのメンバ変数を直接変更する必要がある場合、メソッドの返却値には
    レシーバの型のポインタ（ポインタレシーバ）を宣言する必要がある。
    c.f. https://qiita.com/tikidunpon/items/2d9598f33817a6e99860

--- money_test.go
+++ money_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestMultiplication(t *testing.T) {
 	var five = Dollar{5}
-	five.times(2)
+	five.Times(2)
 
 	if x := five.amount; 10 != x {
 		t.Errorf("Dollar#time(%v) = %v, want %v", 2, x, 10)
