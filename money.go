5767ee4aabaa14ef1c41f3c5900e7bcf1f52b247
msfukui <msfukui@gmail.com>
2019-12-22 10:12:44 +0900

    Add Dollar's constractor and #Times Creates a new Dollar and returns its pointer

--- money.go
+++ money.go
@@ -5,7 +5,14 @@ type Dollar struct {
 	amount int
 }
 
+// NewDollar is Dollar's constructor
+func NewDollar(amount int) *Dollar {
+	c := new(Dollar)
+	c.amount = amount
+	return c
+}
+
 // Times multiplies a Dollar amount by the specified value.
-func (d *Dollar) Times(multiplier int) {
-	d.amount *= multiplier
+func (d *Dollar) Times(multiplier int) *Dollar {
+	return NewDollar(d.amount * multiplier)
 }
